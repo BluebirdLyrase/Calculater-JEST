ef5b581fa703bf0ef8fb862ad5d8fcc39ee4daa4
You@I.com <You@I.com>
2019-11-02 15:01:16 +0700

    gffd

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function add(x,y){
+    return x+y;
+}
+
+function minus(x,y){
+    return x-y;
+}
+
+function multiply(x,y){
+    return x*y;
+}
+
+function devide(x,y){
+    return x/y;
+}
+
+const calculater = {add,minus,multiply,devide}
+module.export = calculator;
